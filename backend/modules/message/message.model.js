e5cb95f5915cbbbe1346dbb7292c7ecede13fc3b
sifatullahsu <personal.sifat@gmail.com>
2024-09-11 17:28:38 +0600

    Message schema

---
+++ backend/modules/message/message.model.js
@@ -0,0 +1,57 @@
+import { model, Schema, Types } from "mongoose";
+
+const schema = new Schema(
+  {
+    conversation: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "Thread",
+    },
+    sender: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    reply: {
+      type: Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
+    message: {
+      type: String,
+      default: "",
+    },
+    files: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        size: {
+          type: Number,
+          required: true,
+        },
+        type: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+export const Message = model("Message", schema);
