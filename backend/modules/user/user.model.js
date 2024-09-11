221df775f517f93d6a8a1029d1b3d9bfe92b534e
sifatullahsu <personal.sifat@gmail.com>
2024-09-11 15:23:37 +0600

    user schema

---
+++ backend/modules/user/user.model.js
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+export const User = model("User", schema);
