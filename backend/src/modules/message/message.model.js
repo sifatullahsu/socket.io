37bdf8a4dc68cf17dcc77521aa67feb8ef8c3cd3
sifatullahsu <personal.sifat@gmail.com>
2024-09-16 12:09:51 +0600

    update message schema

--- backend/src/modules/message/message.model.js
+++ backend/src/modules/message/message.model.js
@@ -2,73 +2,29 @@ import { model, Schema, Types } from "mongoose";
 
 const schema = new Schema(
   {
-    conversation: {
-      type: Types.ObjectId,
-      required: true,
-      ref: "Thread",
-    },
-    sender: {
-      type: Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-    reply: {
-      type: Types.ObjectId,
-      ref: "Message",
-      default: null,
-    },
-    message: {
-      type: String,
-      default: "",
-    },
+    conversation: { type: Types.ObjectId, required: true, ref: "Thread" },
+    sender: { type: Types.ObjectId, required: true, ref: "User" },
+    reply: { type: Types.ObjectId, ref: "Message", default: null },
+    message: { type: String, default: "" },
     files: [
       {
-        name: {
-          type: String,
-          required: true,
-        },
-        size: {
-          type: Number,
-          required: true,
-        },
-        type: {
-          type: String,
-          required: true,
-        },
-        url: {
-          type: String,
-          required: true,
-        },
-        id: {
-          type: String,
-          required: true,
-        },
+        id: { type: String, required: true },
+        name: { type: String, required: true },
+        size: { type: Number, required: true },
+        type: { type: String, required: true },
+        url: { type: String, required: true },
       },
     ],
     removed: {
-      soft: [
-        {
-          type: Types.ObjectId,
-          required: true,
-          ref: "User",
-        },
-      ],
-      hard: {
-        type: Boolean,
-        default: false,
-        required: true,
-      },
+      soft: [{ type: Types.ObjectId, required: true, ref: "User" }],
+      hard: { type: Boolean, default: false, required: true },
     },
   },
   {
     timestamps: true,
     versionKey: false,
-    toJSON: {
-      virtuals: true,
-    },
-    toObject: {
-      virtuals: true,
-    },
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
 
